a8fc0f4992a299411d20c806acea02ea698a08e4
UdaraDewshan <dewudara@gmail.com>
2025-12-09 02:24:22 +0530

    Add Login Page FXML

--- src/main/java/controller/LoginPageController.java
+++ src/main/java/controller/LoginPageController.java
@@ -1,5 +1,42 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginPageController {
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private CheckBox chkRemember;
+
+    @FXML
+    private Label lblMessage;
+
+    @FXML
+    private Hyperlink linkSignUp;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    void onLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSignUp(ActionEvent event) {
+
+    }
 
 }

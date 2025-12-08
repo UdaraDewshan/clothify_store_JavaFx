713ecae6852dd79d3b1f4210eaf4393db2d09344
UdaraDewshan <dewudara@gmail.com>
2025-12-08 22:45:16 +0530

    Add mysql connector-j dependency

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Starter extends Application {
@@ -8,6 +10,7 @@ public class Starter extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/LoginPage.fxml"))));
+        stage.show();
     }
 }
